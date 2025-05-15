0432d0bd39a3527a350151078f08ea024086cde3
Albury <Catalina.Albury@dfo-mpo.gc.ca>
2025-05-15 10:59:15 -0700

    edit webscraping fn and cache data

--- functions/webscraping_fns.R
+++ functions/webscraping_fns.R
@@ -1,6 +1,15 @@
 
 # Functions used in webscraping for Visualizations.qmd
 
+# Define root directory for TBS website with salary data
+url <- "https://www.canada.ca/en/treasury-board-secretariat/services/innovation/human-resources-statistics/diversity-inclusion-statistics/distribution-public-service-canada-employees-designated-sub-group-salary-range-"
+
+
+
+# List of years to get data for 
+years <- c("2023", "2022", "2021", "2020", "2019", "2018", "2017")
+
+
 # Function for removing extra characters
 remove_bullish <- function(bullish_cols) {
   as.numeric(gsub(
@@ -17,15 +26,48 @@ remove_spaces <- function(spacey_cols) {
        x = spacey_cols)
 }
 
+# Function for cleaning up tables 
+# Clean up totals rows
+table_cleanup <- function(messy_cols){
+  function(.data){
+    .data |> 
+      
+     # Clean up totals column
+       mutate({{ messy_cols }} := ifelse(grepl("Total", {{ messy_cols }}),
+                                   "Total",
+                                   {{ messy_cols}})) |>
+      
+      # Remove rows with salary or table
+      filter(!grepl("^Salary|^Table|^Footnote", {{messy_cols}})) |>
+      
+      
+      mutate(across(where(is.character), ~na_if(., "Table 1 Footnote *"))) |>
+      
+      
+      mutate_at(vars(matches("count")), remove_bullish) |> # remove extra characters from counts
+      
+      
+      mutate_at(vars(matches("salary")), remove_spaces) # remove extra spaces from counts
+  }
+}
+
+
+
+
+
 # FIXME set this up so that user is asked if cached data should be used
 # HERE working on getting function working. Need to match salary categories for 2023
-# 2021 all values all -inf - FIXME 
+# FIXME 2021 all values all -inf - 
 # Function to get salary data from website over years
-get_salary_data <- function(Year, 
-                            cache_dir = here("cached_data"), 
-                            refresh = NULL) # do not refresh cache by default 
+get_salary_data <- function(Year,
+                            cache_dir = here("cached_data"), # place for cached data
+                            refresh = NULL) 
   
   {
+  
+
+  
+  
   # Progress message 
   message(paste("Webscraping", Year, "data."))
 
@@ -36,71 +78,47 @@ get_salary_data <- function(Year,
    year_index <- as.numeric(year_dictionary$index[year_dictionary$year == Year])
   
   
+   # Get vm and black df
+   vm_black_df <- {html_nodes(read_html(paste0( 
+     url,
+     "members-visible-minorities")), 
+     "table") |> 
+       html_table(fill = TRUE)
+   }[[year_index]][c(1:3, 25)] |> 
+     dplyr::rename( # fix col names
+       salary_range = "Salary range ($)",
+       count_all = "All employees",
+       count_black = "Black",
+       count_vm = "Members of visible minorities") |> 
+     
+     table_cleanup(salary_range)()
    
-  # Pull dataset with rvest
-  assign(paste("salary_df", # Create df's for each year (salary_df_year)
-               Year, 
-               sep = "_"), {
-                 merge(x = {
-                   
-                   ## VM and Black employees ----
-                   {html_nodes(read_html(paste0( 
-                     url,
-                     "members-visible-minorities")), 
-                     "table") |> 
-                       html_table(fill = TRUE)
-                   }[[year_index]][c(1:3, 25)] |> # remove unneeded cols
-                     
-                     # retrieve all employees and total vm column
-                     dplyr::rename( # fix col names
-                       salary_range = "Salary range ($)",
-                       count_all = "All employees",
-                       count_black = "Black",
-                       count_vm = "Members of visible minorities")},
-                   y = {
-                     
-                     # Indigenous employees----
-                     {html_nodes(read_html(paste0(url,"indigenous-peoples")), "table") |>
-                         html_table(fill = TRUE)
-                     }[[year_index]][c(1, 11)] |> # retrieve total indigenous column
-                       dplyr::rename(salary_range = "Salary range ($)",
-                                     count_indigenous = "Indigenous Peoples")   },
-                   by = "salary_range",
-                   all = TRUE) |>
-                   merge(y = {
-                     
-                     ## Persons with disabilities ----
-                     {html_nodes(read_html(paste0(url, "persons-disabilities")), "table") |>
-                         html_table(fill = TRUE)
-                     }[[year_index]][c(1, 15)] |> # retrieve total pwd column
-                       
-                       
-                       dplyr::rename(salary_range = "Salary range ($)",
-                                     
-                                     
-                                     count_pwd = "Persons with disabilities")
-                   },
-                   by = "salary_range",
-                   all = TRUE) |>
-                   
-                   # Clean up totals rows
-                   # mutate(salary_range = ifelse(grepl("Total", salary_range),
-                   #                              "Total",
-                   #                              salary_range)) |> 
-                   
-                   # Remove rows with salary or table 
-                  # filter(!grepl("^Salary|^Table|^Footnote", salary_range)) |> 
-                   
-                   
-                   # mutate(across(where(is.character), ~na_if(., "Table 1 Footnote *"))) |> 
-                   
-                   
-                  # mutate_at(vars(matches("count")), remove_bullish) |> # remove extra characters from counts
-                   
-                   
-                   mutate_at(vars(matches("salary")), remove_spaces) # remove extra spaces from counts
-                 
-                 
-               },
-         envir = globalenv())
+   
+   
+   # get indigenous df
+   indig_df <- {html_nodes(read_html(paste0(url,"indigenous-peoples")), "table") |>
+       html_table(fill = TRUE)
+   }[[year_index]][c(1, 11)] |> 
+     dplyr::rename(salary_range = "Salary range ($)",
+                   count_indigenous = "Indigenous Peoples")|> 
+     
+     table_cleanup(salary_range)()
+   
+   # get persons with disabilities df 
+   pwd_df <-  {html_nodes(read_html(paste0(url, "persons-disabilities")), "table") |>
+       html_table(fill = TRUE)
+   }[[year_index]][c(1, 15)] |>
+     dplyr::rename(salary_range = "Salary range ($)",
+                   count_pwd = "Persons with disabilities") |> 
+     
+     table_cleanup(salary_range)()
+   
+   # Merge all 3 df's
+   merged_df <- vm_black_df |> 
+     full_join(indig_df, by = "salary_range") |> 
+     full_join(pwd_df, by = "salary_range") |> 
+     write.csv(file = here(paste0("cached_data/salary_data_", Year, ".csv")), 
+               row.names = FALSE)
+   
 }
+
