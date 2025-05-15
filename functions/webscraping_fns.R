acf78f43515ae8151e2fcde0b84e93e686af88ad
Albury <Catalina.Albury@dfo-mpo.gc.ca>
2025-05-15 11:59:50 -0700

    add cacheing feature

--- functions/webscraping_fns.R
+++ functions/webscraping_fns.R
@@ -57,7 +57,6 @@ table_cleanup <- function(messy_cols){
 
 # FIXME set this up so that user is asked if cached data should be used
 # HERE working on getting function working. Need to match salary categories for 2023
-# FIXME 2021 all values all -inf - 
 # Function to get salary data from website over years
 get_salary_data <- function(Year,
                             cache_dir = here("cached_data"), # place for cached data
@@ -65,60 +64,98 @@ get_salary_data <- function(Year,
   
   {
   
-
+  
+  # Check for cached files for the year 
+  cached_files <- list.files(cache_dir, 
+                             pattern = "\\.csv$", 
+                             full.names = TRUE)
   
   
-  # Progress message 
-  message(paste("Webscraping", Year, "data."))
-
-  # Match years to their index in the data
-  year_dictionary <- data.frame(year = years,
-                                index = c(1:length(years)))
-  # Index for year
-   year_index <- as.numeric(year_dictionary$index[year_dictionary$year == Year])
   
   
-   # Get vm and black df
-   vm_black_df <- {html_nodes(read_html(paste0( 
-     url,
-     "members-visible-minorities")), 
-     "table") |> 
-       html_table(fill = TRUE)
-   }[[year_index]][c(1:3, 25)] |> 
-     dplyr::rename( # fix col names
-       salary_range = "Salary range ($)",
-       count_all = "All employees",
-       count_black = "Black",
-       count_vm = "Members of visible minorities") |> 
-     
-     table_cleanup(salary_range)()
+  # Ask user if refresh is not specified and cache exists
+  if (is.null(refresh) && file.exists(here(paste0("cached_data/salary_data_", Year, ".csv")))) {
+    answer <- readline("Cached tables found. Refresh cache? (y/n): ")
+    refresh <- tolower(answer) == "y"
+  } else if (is.null(refresh)) {
+    refresh <- TRUE # No cache exists, must scrape
+  }
+  
+  
+  
+  
+  # If cache exists and no refresh, load from cache
+  if (file.exists(here(paste0("cached_data/salary_data_", Year, ".csv"))) && !refresh) {
+    message("Loading tables from cache...")
+    
+    # read csv for file from cache and assign in global environment
+    assign(paste0("salary_data_", Year),
+           {read.csv(here(paste0("cached_data/salary_data_", Year, ".csv")))},
+           envir = globalenv())
+  }
+  
+  if(refresh == TRUE){
+    # Progress message 
+    message(paste("Webscraping", Year, "data."))
+    
+    # Match years to their index in the data
+    year_dictionary <- data.frame(year = years,
+                                  index = c(1:length(years)))
+    # Index for year
+    year_index <- as.numeric(year_dictionary$index[year_dictionary$year == Year])
+    
+    
+    # Get vm and black df
+    vm_black_df <- {html_nodes(read_html(paste0( 
+      url,
+      "members-visible-minorities")), 
+      "table") |> 
+        html_table(fill = TRUE)
+    }[[year_index]][c(1:3, 25)] |> 
+      dplyr::rename( # fix col names
+        salary_range = "Salary range ($)",
+        count_all = "All employees",
+        count_black = "Black",
+        count_vm = "Members of visible minorities") |> 
+      
+      table_cleanup(salary_range)()
+    
+    
+    
+    # get indigenous df
+    indig_df <- {html_nodes(read_html(paste0(url,"indigenous-peoples")), "table") |>
+        html_table(fill = TRUE)
+    }[[year_index]][c(1, 11)] |> 
+      dplyr::rename(salary_range = "Salary range ($)",
+                    count_indigenous = "Indigenous Peoples")|> 
+      
+      table_cleanup(salary_range)()
+    
+    # get persons with disabilities df 
+    pwd_df <-  {html_nodes(read_html(paste0(url, "persons-disabilities")), "table") |>
+        html_table(fill = TRUE)
+    }[[year_index]][c(1, 15)] |>
+      dplyr::rename(salary_range = "Salary range ($)",
+                    count_pwd = "Persons with disabilities") |> 
+      
+      table_cleanup(salary_range)()
+    
+    # Merge all 3 df's
+    merged_df <- vm_black_df |> 
+      full_join(indig_df, by = "salary_range") |> 
+      full_join(pwd_df, by = "salary_range") 
+    
+    # Write to csv
+    write.csv(merged_df, file = here(paste0("cached_data/salary_data_", Year, ".csv")), 
+                row.names = FALSE)
+    
+    # Load tables to environment
+    message("Loading tables from cache...")
+    assign(paste0("salary_data_", Year),
+           {merged_df},
+           envir = globalenv())
    
-   
-   
-   # get indigenous df
-   indig_df <- {html_nodes(read_html(paste0(url,"indigenous-peoples")), "table") |>
-       html_table(fill = TRUE)
-   }[[year_index]][c(1, 11)] |> 
-     dplyr::rename(salary_range = "Salary range ($)",
-                   count_indigenous = "Indigenous Peoples")|> 
-     
-     table_cleanup(salary_range)()
-   
-   # get persons with disabilities df 
-   pwd_df <-  {html_nodes(read_html(paste0(url, "persons-disabilities")), "table") |>
-       html_table(fill = TRUE)
-   }[[year_index]][c(1, 15)] |>
-     dplyr::rename(salary_range = "Salary range ($)",
-                   count_pwd = "Persons with disabilities") |> 
-     
-     table_cleanup(salary_range)()
-   
-   # Merge all 3 df's
-   merged_df <- vm_black_df |> 
-     full_join(indig_df, by = "salary_range") |> 
-     full_join(pwd_df, by = "salary_range") |> 
-     write.csv(file = here(paste0("cached_data/salary_data_", Year, ".csv")), 
-               row.names = FALSE)
+   }
    
 }
 
